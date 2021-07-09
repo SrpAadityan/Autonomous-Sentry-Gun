7a366f1a6e2439488bf781006c16d58886f8c6ab
SrpAadityan <srp.aadi@gmail.com>
2021-07-09 22:41:38 +0530

    update src files

---
+++ servoControl.py
@@ -0,0 +1,20 @@
+import time
+import wiringpi
+ 
+# use 'GPIO naming'
+wiringpi.wiringPiSetupGpio()
+ 
+# set #18 and #13 to be a PWM output
+wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+wiringpi.pinMode(13, wiringpi.GPIO.PWM_OUTPUT)
+ 
+# set the PWM mode to milliseconds stype
+wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+ 
+# divide down clock
+wiringpi.pwmSetClock(192)
+wiringpi.pwmSetRange(2000)
+
+def move(pulse,servo):
+        #pulse=(50+200*float(angle)/180)
+        wiringpi.pwmWrite(servo, pulse)
